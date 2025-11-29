81bd1abdb0bea3fef57376add88bef30d10d68e7
Kait Rice <kaitnrice@gmail.com>
2025-11-29 13:40:00 -0800

    change model & thinking logic

--- backend/app/gemini.js
+++ backend/app/gemini.js
@@ -9,9 +9,14 @@ export default async ({ thinking, prompt, settings }) => {
     if (!prompt) throw new Error("Error: prompt cannot be null.");
 
     const response = await ai.models.generateContent({
-        model: thinking ? "gemini-2.5-pro" : "gemini-2.5-flash-lite",
+        model: "gemini-2.5-flash",
         contents: prompt,
-        config: settings
+        config: {
+            ...settings,
+            thinkingConfig: {
+                thinkingBudget: thinking ? -1 : 0
+            }
+        }
     });
 
     return response.text;
